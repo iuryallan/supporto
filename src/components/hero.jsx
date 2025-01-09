ceb1a27ed813ee27c2a2732c697f732097ca8fad
DiegoAraaujo <diegoaraaujo8@gmail.com>
2025-01-09 16:46:47 -0300

    modificações na seção hero e no cabeçalho.

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -4,7 +4,7 @@ import ImgHero from '../assets/img-hero.svg'
 
 function hero(){
   return(
-    <section>
+    <section className="section-hero">
       <div className="content-hero">
         <h2>
           Nem toda tempestade precisa ser enfrentada sozinha. Procure ajuda.
@@ -12,10 +12,10 @@ function hero(){
         <p>
           Somos uma plataforma de Apoio Psicológico Comunitário. Oferecemos profissionais qualificados e serviço acessível.
         </p>
-        <Button label='Quero ajuda'/>
+        <Button className="help-me" label='Quero ajuda'/>
       </div>
       <div>
-        <img src={ImgHero} alt='duas pessoas abraçadas' className='img-hero'/>;
+        <img src={ImgHero} alt='duas pessoas abraçadas' className='img-hero'/>
       </div>
     </section>
   );
