5b941ee33f12c8430bc4e2d87b19d61942b4a794
Fabrício M <fabmartins272@gmail.com>
2025-03-12 22:34:07 -0300

    Completando o Schema Prisma

---
+++ back/prisma/migrations/20250313013159_init/migration.sql
@@ -0,0 +1,116 @@
+-- CreateEnum
+CREATE TYPE "Status" AS ENUM ('Confirmado', 'Cancelado', 'Finalizado');
+
+-- CreateEnum
+CREATE TYPE "Gender" AS ENUM ('Masculino', 'Feminino', 'Outro');
+
+-- CreateEnum
+CREATE TYPE "Type" AS ENUM ('Administrador', 'Paciente', 'Profissional');
+
+-- CreateTable
+CREATE TABLE "Usuario" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "tipo" "Type" NOT NULL,
+    "senha" TEXT NOT NULL,
+
+    CONSTRAINT "Usuario_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Profissional" (
+    "id" SERIAL NOT NULL,
+    "nome" TEXT NOT NULL,
+    "matricula_profissional" TEXT NOT NULL,
+    "foto_perfil" TEXT,
+    "quant_atend_gratis" INTEGER NOT NULL,
+    "faixa_etaria" TEXT NOT NULL,
+    "cidade" TEXT NOT NULL,
+    "estado" TEXT NOT NULL,
+    "genero" "Gender" NOT NULL,
+    "idade" INTEGER NOT NULL,
+    "usuarioId" INTEGER NOT NULL,
+
+    CONSTRAINT "Profissional_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Paciente" (
+    "id" SERIAL NOT NULL,
+    "nome" TEXT NOT NULL,
+    "genero" "Gender" NOT NULL,
+    "data_nasc" DATE NOT NULL,
+    "motivo_terapia" TEXT NOT NULL,
+    "medicamentos" TEXT NOT NULL,
+    "historico_familiar" TEXT NOT NULL,
+    "principal_queixa" TEXT NOT NULL,
+    "usuarioId" INTEGER NOT NULL,
+
+    CONSTRAINT "Paciente_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Evolucao_clinica" (
+    "id" SERIAL NOT NULL,
+    "data" TIMESTAMP(3) NOT NULL,
+    "relato_atendimento" TEXT NOT NULL,
+    "ajustes_tratamento" TEXT NOT NULL,
+    "pacienteId" INTEGER NOT NULL,
+    "profissionalId" INTEGER,
+
+    CONSTRAINT "Evolucao_clinica_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Atendimento" (
+    "id" SERIAL NOT NULL,
+    "data" TIMESTAMP(3) NOT NULL,
+    "status" "Status" NOT NULL,
+    "pacienteId" INTEGER NOT NULL,
+    "profissionalId" INTEGER NOT NULL,
+
+    CONSTRAINT "Atendimento_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Grupo_apoio" (
+    "id" SERIAL NOT NULL,
+    "link" TEXT NOT NULL,
+    "descricao" TEXT NOT NULL,
+
+    CONSTRAINT "Grupo_apoio_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Depoimento" (
+    "id" SERIAL NOT NULL,
+    "texto" TEXT NOT NULL,
+    "data" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "usuarioId" INTEGER NOT NULL,
+
+    CONSTRAINT "Depoimento_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Usuario_email_key" ON "Usuario"("email");
+
+-- AddForeignKey
+ALTER TABLE "Profissional" ADD CONSTRAINT "Profissional_usuarioId_fkey" FOREIGN KEY ("usuarioId") REFERENCES "Usuario"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Paciente" ADD CONSTRAINT "Paciente_usuarioId_fkey" FOREIGN KEY ("usuarioId") REFERENCES "Usuario"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Evolucao_clinica" ADD CONSTRAINT "Evolucao_clinica_pacienteId_fkey" FOREIGN KEY ("pacienteId") REFERENCES "Paciente"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Evolucao_clinica" ADD CONSTRAINT "Evolucao_clinica_profissionalId_fkey" FOREIGN KEY ("profissionalId") REFERENCES "Profissional"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Atendimento" ADD CONSTRAINT "Atendimento_pacienteId_fkey" FOREIGN KEY ("pacienteId") REFERENCES "Paciente"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Atendimento" ADD CONSTRAINT "Atendimento_profissionalId_fkey" FOREIGN KEY ("profissionalId") REFERENCES "Profissional"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Depoimento" ADD CONSTRAINT "Depoimento_usuarioId_fkey" FOREIGN KEY ("usuarioId") REFERENCES "Usuario"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
