3c6f7cacbb30f2768a9aa9a0005ffae92169ef4a
guisholanda <116400205+guisholanda@users.noreply.github.com>
2025-03-14 18:52:12 -0300

    adição do botão de sair e ajuste estilos na página de avaliação

--- src/pages/avaliacao-atendimento/Avaliacao.jsx
+++ src/pages/avaliacao-atendimento/Avaliacao.jsx
@@ -1,14 +1,20 @@
 import React, { useState } from "react";
+import { useNavigate } from 'react-router-dom';
 import "./avaliacao.css";
 import Estrela from "./components/estrela";
 
 
 const Avaliacao = () => {
+  const navigate = useNavigate();
+
+  const handleClose = () => {
+    navigate('/Agendamento');
+  };
   const [rating, setRating] = useState(0);
   const [review, setReview] = useState("");
 
   return (
-    <div className="container-review">
+    <div className="container-review">     
         <div className="card-review">
             <p className="info-text-review">
                 Sua avaliação é anônima, mas pode aparecer publicamente sem que as pessoas saibam que foi você.
@@ -25,6 +31,9 @@ const Avaliacao = () => {
             />
         <button onClick className="submit-btn">
         Enviar avaliação
+        </button>
+        <button onClick={handleClose} className="submit-btn">
+          Sair
         </button>
       </div>
     </div>
