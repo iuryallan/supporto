e28eee8a5c528d7d472823170eb47b2ecf161640
DiegoAraaujo <diegoaraaujo8@gmail.com>
2025-03-21 16:22:48 -0300

    começando a integração

--- front/src/pages/lobby/components/avaliacao.jsx
+++ front/src/pages/lobby/components/avaliacao.jsx
@@ -1,21 +1,63 @@
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
 import CardOpiniao from "./cardOpiniao";
-import './avaliacao.css'
+import './avaliacao.css';
 
-import './avaliacao.css'
-function Avaliacao(){
-    return (
-        <section id = 's2' className="secaoAvaliacao">
-            <div>
-                <div className="tituloAvaliacao">
-                    <h2>Qual a opinião dos nossos usuários?</h2>
-                </div>
-                <div className="conteinerCards">
-                    <CardOpiniao nome='xxxx' profissional='xxxxx' opiniao = 'xxxxx'/>
-                    <CardOpiniao nome='xxxx' profissional='xxxxx' opiniao = 'xxxxx'/>
-                    <CardOpiniao nome='xxxxxx' profissional='xxxxx' opiniao = 'xxxx'/>
-                </div>
-            </div>
-        </section>
-    )
-} 
+function Avaliacao() {
+  const [avaliacaoReal, setAvaliacaoReal] = useState(null);
+
+  const avaliacaoGenerica = {
+    nome: "Usuário Genérico",
+    opiniao: "Este é um comentário genérico para preencher o card.",
+  };
+
+
+  useEffect(() => {
+    Axios.get('http://localhost:3007/depoimentos')
+      .then((response) => {
+        console.log(response.data[0])
+        setAvaliacaoReal(response.data[0]);
+      })
+      .catch((error) => {
+        console.error('Erro ao carregar as avaliações:', error);
+      });
+  }, []);
+
+  return (
+    <section id='s2' className="secaoAvaliacao">
+      <div>
+        <div className="tituloAvaliacao">
+          <h2>Qual a opinião dos nossos usuários?</h2>
+        </div>
+        <div className="conteinerCards">
+          {avaliacaoReal ? (
+            <CardOpiniao
+              nome={avaliacaoReal.data}
+              opiniao={avaliacaoReal.texto}
+            />
+          ) : (
+            <CardOpiniao
+              nome="Carregando..."
+              opiniao="Carregando avaliação..."
+            />
+          )}
+
+          <CardOpiniao
+            nome={avaliacaoGenerica.nome}
+            opiniao={avaliacaoGenerica.opiniao}
+          />
+          <CardOpiniao
+            nome={avaliacaoGenerica.nome}
+            opiniao={avaliacaoGenerica.opiniao}
+          />
+          <CardOpiniao
+            nome={avaliacaoGenerica.nome}
+            opiniao={avaliacaoGenerica.opiniao}
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
+
 export default Avaliacao;
