545bb1b264abf5ccc862384911753b5bcd2b26b6
DiegoAraaujo <diegoaraaujo8@gmail.com>
2025-03-22 19:56:24 -0300

    avaliações exibidas no lobby agora estão integradas com o banco de dados!

--- front/src/pages/lobby/components/avaliacao.jsx
+++ front/src/pages/lobby/components/avaliacao.jsx
@@ -1,59 +1,48 @@
 import React, { useState, useEffect } from "react";
 import Axios from "axios";
 import CardOpiniao from "./cardOpiniao";
-import './avaliacao.css';
+import "./avaliacao.css";
 
 function Avaliacao() {
-  const [avaliacaoReal, setAvaliacaoReal] = useState(null);
+  const [avaliacaoReal, setAvaliacaoReal] = useState([]);
 
   const avaliacaoGenerica = {
     nome: "Usuário Genérico",
     opiniao: "Este é um comentário genérico para preencher o card.",
   };
 
-
   useEffect(() => {
-    Axios.get('http://localhost:3007/depoimentos')
+    Axios.get("http://localhost:3007/depoimentos")
       .then((response) => {
-        console.log(response.data[0])
-        setAvaliacaoReal(response.data[0]);
+        const embaralhado = response.data.sort(() => Math.random() - 0.5);
+        setAvaliacaoReal(embaralhado.slice(0, 4));
       })
       .catch((error) => {
-        console.error('Erro ao carregar as avaliações:', error);
+        console.error("Erro ao carregar as avaliações:", error);
       });
   }, []);
 
   return (
-    <section id='s2' className="secaoAvaliacao">
+    <section id="s2" className="secaoAvaliacao">
       <div>
         <div className="tituloAvaliacao">
           <h2>Qual a opinião dos nossos usuários?</h2>
         </div>
         <div className="conteinerCards">
-          {avaliacaoReal ? (
-            <CardOpiniao
-              nome={avaliacaoReal.data}
-              opiniao={avaliacaoReal.texto}
-            />
+          {avaliacaoReal.length > 0 ? (
+            avaliacaoReal.map((avaliacao, index) => (
+              <CardOpiniao
+                key={index}
+                nome={avaliacao.data}
+                opiniao={avaliacao.texto}
+              />
+            ))
           ) : (
             <CardOpiniao
               nome="Carregando..."
               opiniao="Carregando avaliação..."
             />
           )}
-
-          <CardOpiniao
-            nome={avaliacaoGenerica.nome}
-            opiniao={avaliacaoGenerica.opiniao}
-          />
-          <CardOpiniao
-            nome={avaliacaoGenerica.nome}
-            opiniao={avaliacaoGenerica.opiniao}
-          />
-          <CardOpiniao
-            nome={avaliacaoGenerica.nome}
-            opiniao={avaliacaoGenerica.opiniao}
-          />
         </div>
       </div>
     </section>
